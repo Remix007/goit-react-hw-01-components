4529c617d471f1a6fedacb2715163ee9a3d97c3d
Remix007 <forvard16back@gmail.com>
2023-02-08 20:51:05 +0200

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,12 @@
+import Profile from './Profile/Profile';
+import Statistics from './Statistics/Statistics';
+import FriendList from './FriendList/FriendList';
+import TransactionHistory from './Transaction/TransactionHistory';
+import user from '../Data/user.json';
+import data from '../Data/data.json';
+import friends from '../Data/friends.json';
+import transactions from '../Data/transactions.json';
+
 export const App = () => {
   return (
     <div
